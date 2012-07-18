0e05e7cce5643b340a8a4cfa7194aa830e2ce6d0
Alex Chang <alexc804@gmail.com>
2012-07-19 00:17:41 +0800

    -fix path -upgrade apk version
    Change-Id: I9b717a283aa9bd0bc7b4d69874dc65a2d9ed6589

--- milestone2-vendor.mk
+++ milestone2-vendor.mk
@@ -171,9 +171,9 @@ PRODUCT_COPY_FILES += \
 
 #app
 PRODUCT_COPY_FILES += \
-	vendor/motorola/jordan/app/MotoPhonePortal.apk:system/app/MotoPhonePortal.apk \
-	vendor/motorola/jordan/app/AudioEffectSettings.apk:system/app/AudioEffectSettings.apk \
-	vendor/motorola/jordan/app/CompassCalibrate.apk:system/app/CompassCalibrate.apk \
+	vendor/motorola/milestone2/app/MotoPhonePortal.apk:system/app/MotoPhonePortal.apk \
+	vendor/motorola/milestone2/app/AudioEffectSettings.apk:system/app/AudioEffectSettings.apk \
+	vendor/motorola/milestone2/app/CompassCalibrate.apk:system/app/CompassCalibrate.apk \
 
 #extra
 PRODUCT_COPY_FILES += \
